ec936c912402e537eb307b7474881f40a0c8b77a
Mathieu Debras <debras.mathieu@gmail.com>
2018-10-10 15:52:23 +0200

    ex 4

---
+++ training1.js
@@ -0,0 +1,6 @@
+let clickEvent = () => {
+    let nom = document.getElementById("nom").value;
+    let prenom = document.getElementById("prenom").value;
+    let ville = document.getElementById("ville").value;
+    return alert("Nom : " + nom + "\nPrenom : " + prenom + "\nVille : " + ville);
+}

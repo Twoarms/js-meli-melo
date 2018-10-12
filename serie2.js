b133eb3b7d96d7a299809094df8a7533fe6867c2
Twoarms <debras.mathieu@gmail.com>
2018-10-12 10:55:35 +0200

    ex 8

--- serie2.js
+++ serie2.js
@@ -95,3 +95,29 @@ let faireChoix = () => {
             alert(`Pardon, que voulez-vous ?`)
     }
 }
+
+let jourDeLaSemaine = () => {
+    switch (new Date().getDay()) {
+        case 0:
+            alert("Dimanche");
+            break;
+        case 1:
+            alert("Lundi");
+            break;
+        case 2:
+            alert("Mardi");
+            break;
+        case 3:
+            alert("Mercredi");
+            break;
+        case 4:
+            alert("Jeudi");
+            break;
+        case 5:
+            alert("Vendredi");
+            break;
+        case 6:
+            alert("Samedi");
+    }
+}
+
